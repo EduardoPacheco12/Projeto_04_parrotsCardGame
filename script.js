7d9cf5daba02bb607aaf20e4fb6ca088a03c39e5
Eduardo <eduardooliveirapacheco131@gmail.comtouch meuTexto.txtcd>
2022-04-11 04:03:15 -0300

    Clique e carta virando

--- script.js
+++ script.js
@@ -12,14 +12,24 @@ function iniciarJogo() {
         console.log(numeroCartas);
         iniciarJogo();
     }
-    const arrayGifs = ["fiestaparrot.gif", "fiestaparrot.gif", "unicornparrot.gif", "unicornparrot.gif", "bobrossparrot.gif", "bobrossparrot.gif", "revertitparrot.gif",  "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "metalparrot.gif", "metalparrot.gif"]
+    const arrayGifs = ["fiestaparrot.gif", "fiestaparrot.gif", "unicornparrot.gif", "unicornparrot.gif", "bobrossparrot.gif", "bobrossparrot.gif", "revertitparrot.gif",  "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "metalparrot.gif", "metalparrot.gif"];
+    const arrayGifs2 = [];
+    for(let i = 0; i < numeroCartas; i++) {
+        arrayGifs2[i] = arrayGifs[i];
+    }
+    arrayGifs2.sort(comparador);
+
     const lista = document.querySelector(".conteudo");
     lista.innerHTML = "";
-    for(let i = 0; i < numeroCartas; i++) {
+    for(let j = 0; j < numeroCartas; j++) {
         lista.innerHTML += `
-        <div class="carta" onclick="virarCarta()">
-        <img class="escondido" src="./Images/front.png">
-        <img src="./Images/${arrayGifs[i]}">
+        <div class="carta" onclick=virarCarta(this)>
+            <div class="face frente">
+                <img src="./Images/front.png">
+            </div>
+            <div class="face verso">
+                <img src="./Images/${arrayGifs2[j]}">
+            </div>
         </div>
         `;
     }
@@ -28,5 +38,13 @@ function iniciarJogo() {
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+
+function virarCarta(elemento) {
+    elemento.classList.add('virar');
+    let cartasViradas = document.querySelectorAll(".virar");
+    console.log(cartasViradas);
+    
+}
+
 
 iniciarJogo();
