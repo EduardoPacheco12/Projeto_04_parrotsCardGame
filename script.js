42d83b24ad1b52e439a6fd7e6d10178d3add3e01
Eduardo <eduardooliveirapacheco131@gmail.comtouch meuTexto.txtcd>
2022-04-11 12:01:45 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const lista2 = [];
+const arrayGifs = ["fiestaparrot.gif", "fiestaparrot.gif", "unicornparrot.gif", "unicornparrot.gif", "bobrossparrot.gif", "bobrossparrot.gif", "revertitparrot.gif",  "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "metalparrot.gif", "metalparrot.gif"];
+const arrayGifs2 = [];
 let numeroCartas = 0;
+let primeiraCarta;
+let segundaCarta;
+let primeiroTexto;
+let segundoTexto;
+let cartas;
+let finalizou;
+let totalCartas;
+let recomecarJogo;
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 function iniciarJogo() {
     numeroCartas = prompt("Quantas cartas você quer utilizar?(escolha um valor par entre 4 à 14 cartas)")
@@ -12,18 +27,16 @@ function iniciarJogo() {
         console.log(numeroCartas);
         iniciarJogo();
     }
-    const arrayGifs = ["fiestaparrot.gif", "fiestaparrot.gif", "unicornparrot.gif", "unicornparrot.gif", "bobrossparrot.gif", "bobrossparrot.gif", "revertitparrot.gif",  "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "metalparrot.gif", "metalparrot.gif"];
-    const arrayGifs2 = [];
     for(let i = 0; i < numeroCartas; i++) {
         arrayGifs2[i] = arrayGifs[i];
     }
     arrayGifs2.sort(comparador);
 
-    const lista = document.querySelector(".conteudo");
-    lista.innerHTML = "";
+    cartas = document.querySelector(".conteudo");
+    cartas.innerHTML = "";
     for(let j = 0; j < numeroCartas; j++) {
-        lista.innerHTML += `
-        <div class="carta" onclick=virarCarta(this)>
+        cartas.innerHTML += `
+        <div onclick=virarCarta(this)>
             <div class="face frente">
                 <img src="./Images/front.png">
             </div>
@@ -35,16 +48,37 @@ function iniciarJogo() {
     }
 }
 
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-
 function virarCarta(elemento) {
-    elemento.classList.add('virar');
-    let cartasViradas = document.querySelectorAll(".virar");
-    console.log(cartasViradas);
-    
+    elemento.classList.add("virar");
+    textoDiv = elemento.innerHTML;
+    lista2.push(textoDiv);
+    if (lista2.length % 2 !== 0) {
+        primeiraCarta = elemento;
+    }
+    if (lista2.length % 2 === 0) {
+        segundaCarta = elemento;
+    }
+    if (lista2.length % 2 === 0) {
+        primeiroTexto = lista2[lista2.length - 2];
+        segundoTexto = lista2[lista2.length - 1];
+        if (primeiroTexto !== segundoTexto) {
+            setTimeout(virarNovamente, 1000);
+        }
+    }
+    finalizou = document.querySelectorAll(".conteudo .virar");
+    totalCartas = document.querySelectorAll(".conteudo > div");
+    if (finalizou.length === totalCartas.length) {
+        setTimeout(jogoVencido, 1000);
+    }
 }
 
+function virarNovamente() {
+    primeiraCarta.classList.remove("virar");
+    segundaCarta.classList.remove("virar");
+}
+
+function jogoVencido() {
+    alert(`Parabéns, você ganhou em ${lista2.length} jogadas!!!`);
+}
 
 iniciarJogo();
